52d9e09422e7e8f1d0f10ef9c49d542ad13a7b2b
Andrei Mahalean <andrei@maha.nz>
2024-08-06 14:38:52 +1200

    add workflow for deployment
    update
    
    update
    
    run workflow every 5 minutes
    
    Update http-status-check.yml
    
    Update http-status-check.yml
    
    update deploy script
    
    update workflow
    
    update
    
    no strict host checking
    
    small config updates
    
    update action to also support push

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-USER=caddy
-HOST=blog.maha.nz
+HOST=blog
 DIR=/var/www/html/   # the directory where your website files should go
 
-hugo --source=./src && rsync -avz --delete src/public/ ${USER}@${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
+hugo --source=./src && rsync -avz --delete src/public/ ${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
 
 exit 0
 
